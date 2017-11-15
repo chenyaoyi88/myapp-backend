983181247abdf6603193b27380a74f2b3b12c36c
chenyaoyi88 <152296058@qq.com>
2017-11-15 23:33:16 +0800

    关于jsonwebtoken微小的调整

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   // JSON Web Token 加密密钥设置
   secret: 'secret_test', 
+  expireTime: 3600,
   database: 'mongodb://127.0.0.1:27017/cyy'
 };
--- routes/check.js
+++ routes/check.js
@@ -7,7 +7,7 @@ const colors = require('colors');
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
-  const token = req.headers.token;  
+  const token = req.headers.token;
   // console.log('***********'.yellow);
   // console.log(token);
   // console.log('***********'.yellow);
@@ -17,21 +17,10 @@ router.post('/', function (req, res, next) {
      * jwt.verify(token, secretOrPublicKey, [options, callback])
      * @param token 客户端传过来的验证 token
      * @param secretOrPublicKey 配置文件里面的密钥
-     * @param options 一大堆的验证细节，暂时用到 maxAge 用来给 token 延期
      * @param callback 回调里面有2个参数，一个是 token 是否有效，一个是解码 payload（例如签发 token 的时候我填的 username）  
      */
-    // try {
-    //   const decoded = jwt.verify(token, config.secret, { maxAge: 30 });
-    // } catch(err) {
-    //   if (err) {
-    //     // 时间失效、伪造 => 超时（需要重新登录）
-    //     res.send(status.timeout());
-    //   } else {
-    //     res.send(status.success());
-    //   }
-    // }
 
-    jwt.verify(token, config.secret, { maxAge: '2 days' }, function (err, decoded) {
+    jwt.verify(token, config.secret, function (err, decoded) {
       console.log('***********'.green);
       console.log(err);
       console.log('***********'.green);
